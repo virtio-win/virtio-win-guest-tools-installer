daa5c5b4e2512c65abcb47e3b292e963b98e4e3f
Gal Zaidman <gzaidman@redhat.com>
2019-08-19 09:32:40 +0300

    Remove old ci tests
    In the old installer lev veyde wrote a CI script
    which is used and maintained by qe, I have added
    the script at the beginning of the project as a
    reference, now that we are inserting unitests
    we can remove the old test and let them be managed
    by QE.
    
    Signed-off-by: Gal Zaidman <gzaidman@redhat.com>

--- test/constants.py
+++
@@ -1,5 +0,0 @@
-fqdn = 'integration-engine.eng.lab.tlv.redhat.com'
-port = 443
-user = 'admin@internal'
-passwd = 'qum6net'
-url = 'http://10.35.37.7/cirunner/ci.php?action={action}&hostID={hostID}'
